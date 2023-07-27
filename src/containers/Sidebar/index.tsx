69fcfcb0e6d8b58dd1e66afb14c2350dd736c21b
victor <victor.freire.oliveira@gmail.com>
2023-07-27 12:05:01 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,18 @@
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Title"
+import Avatar from "../../components/avatar"
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/oliveira-victor.png" />
-    <Titulo fontSize={20}>Victor Freire</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Victor Freire</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>oliveira-victor</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Engenheiro Front-end</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
