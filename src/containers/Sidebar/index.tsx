a5e87b57a321df299403d0f18d33ee47d501a8db
victor <victor.freire.oliveira@gmail.com>
2023-07-26 23:08:37 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Title"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/oliveira-victor.png" />
+    <Titulo fontSize={20}>Victor Freire</Titulo>
+  </aside>
+)
+
+
+export default Sidebar
